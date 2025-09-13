a61c688aee5ce67c72fe1bea47f5d4378391d98a
Elijah Massey <emassey0135@gmail.com>
2025-09-13 13:04:53 -0500

    Update Nix flake

--- flake.nix
+++ flake.nix
@@ -15,10 +15,10 @@
         bedrock-connect = (with pkgs;
           stdenv.mkDerivation {
             pname = "bedrock-connect";
-            version = "1.58";
+            version = "1.59";
             src = fetchurl {
-              url = "https://github.com/Pugmatt/BedrockConnect/releases/download/1.58/BedrockConnect-1.0-SNAPSHOT.jar";
-              hash = "sha256-qEOI+K5ySVwgQKmZZpLx7pX6bjsYVurC1zls9lr4D6U=";
+              url = "https://github.com/Pugmatt/BedrockConnect/releases/download/1.59/BedrockConnect-1.0-SNAPSHOT.jar";
+              hash = "sha256-vB0ycSLdoSiEMgO7+pXAGpuUUd29mxC1v+ozXfStaeg=";
             };
             phases = ["installPhase"];
             installPhase = ''
